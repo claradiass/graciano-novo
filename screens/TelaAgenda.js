448c483e8c79fc5311ff835b185e737d95b151d3
dinizhugo <hugodiniztech01@gmail.com>
2025-07-06 22:49:59 -0300

    chore: Mudando o useEffect

--- screens/TelaAgenda.js
+++ screens/TelaAgenda.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import { useState, useEffect } from 'react';
 import { View, TouchableOpacity, Text, StatusBar, Modal, StyleSheet } from 'react-native';
 import { Agenda, LocaleConfig } from 'react-native-calendars';
 import { useNavigation } from '@react-navigation/native';
@@ -63,7 +63,7 @@ function generateEmptyDates(startDate, endDate) {
 
 export default function TelaAgenda({route}) {
   const [agendamentos, setAgendamentos] = useState([]);
-  const [atualizaLista, setAtualizaLista] = useState(true);
+  const [atualizaLista, setAtualizaLista] = useState(0);
   const [modalVisible, setModalVisible] = useState(false);
   const [excluidoModalVisible, setExcluidoModalVisible] = useState(false);
   const [modalVisible2, setModalVisible2] = useState(false);
@@ -109,39 +109,19 @@ export default function TelaAgenda({route}) {
       });
   }
 
-  function remover() {  
-    console.log("Dados antes da exclusão:", data);
-    if (data && data.id){
-    axios.delete(baseUrlAgendamentos + data.id, configAxios)
+  function remover(id) {
+    axios
+      .delete(`${baseUrlAgendamentos}/${id}`, configAxios)
       .then(function (response) {
         if (response.status == 200) {
-          setAtualizaLista(atualizaLista + 1);
-          mostrarMensagemExcluido();
-        } else {
-          alert("Erro", "Houve um erro na comunicação com o servidor!");
+          setAtualizaLista((prev) => prev + 1);
+          setExcluidoModalVisible(false);
         }
-        
       })
       .catch(error => {
-
-        if (error.response) {
-          // A requisição foi feita e o servidor respondeu com um código de status fora do intervalo 2xx
-          console.error('Response data:', error.response.data);
-          console.error('Response status:', error.response.status);
-          console.error('Response headers:', error.response.headers);
-        } else if (error.request) {
-          // A requisição foi feita, mas não houve resposta do servidor
-          console.error('Request data:', error.request);
-        } else {
-          // Alguma coisa aconteceu ao configurar a requisição que disparou um erro
-          console.error('Error message:', error.message);
-        }
-        console.error('Error config:', error.config);
-        
+        console.error(error);
+        alert("Erro ao excluir agendamento!");
       });
-    } else {
-      console.error("Invalid data or data.id is undefined");
-    }
   }
 
   const toggleModal = () => {
@@ -261,11 +241,17 @@ export default function TelaAgenda({route}) {
     );
   };
 
-  // const renderEmptyItem = () => <View style={styles.emptyItem} />;
+  useEffect(() => {
+    if (route.params?.realizarAtualizacao) {
+      atualiza();
+      navigation.setParams({ realizarAtualizacao: false });
+    }
+  }, [route.params?.realizarAtualizacao]);
 
   useEffect(() => {
-    atualiza();    
-  }, [atualizaLista, route.params]);
+    atualiza();
+  }, [atualizaLista]);
+
 
 
   useEffect( () => {
@@ -318,7 +304,7 @@ export default function TelaAgenda({route}) {
               <View style={styles.modalContent}>
                 <Text style={styles.textbotao}>Deseja excluir esse Agendamento?</Text>
                 <View style={styles.bots}>
-                <TouchableOpacity style={styles.bot} onPress={() => remover()}>
+                <TouchableOpacity style={styles.bot} onPress={() => remover(data.id)}>
                   <Text style={styles.textbotao2} >Sim</Text>
                 </TouchableOpacity>
                 <TouchableOpacity style={styles.bot2} onPress={toggleModal}>
@@ -341,7 +327,7 @@ export default function TelaAgenda({route}) {
               <Text style={styles.textbotao}>Agendamento excluído com sucesso!</Text>
               <TouchableOpacity
                 style={styles.bot3}
-                onPress={() => atualiza()}>
+                onPress={() => setExcluidoModalVisible(false)}>
                 <Text style={styles.textbotao2}>Fechar</Text>
               </TouchableOpacity>
             </View>
@@ -478,406 +464,3 @@ const styles = StyleSheet.create({
 
   },
 });
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// import React, { useState, useEffect } from 'react';
-// import { View, TouchableOpacity, Text, StatusBar, Modal, StyleSheet } from 'react-native';
-// import { Agenda, LocaleConfig } from 'react-native-calendars';
-// import { Feather } from '@expo/vector-icons';
-// import { useNavigation } from '@react-navigation/native';
-// import { Card } from 'react-native-paper';
-// import { format } from 'date-fns';
-// import axios from 'axios';
-// import { 
-//   configAxios,
-//   baseUrlagendamentos,
-//   baseUrlAgendamentos
-// } from '../util/constantes';
-
-// LocaleConfig.locales['br'] = {
-//   monthNames: [
-//     'Janeiro',
-//     'Fevereiro',
-//     'Março',
-//     'Abril',
-//     'Maio',
-//     'Junho',
-//     'Julho',
-//     'Agosto',
-//     'Setembro',
-//     'Outubro',
-//     'Novembro',
-//     'Dezembro',
-//   ],
-//   monthNamesShort: [
-//     'Jan',
-//     'Fev',
-//     'Mar',
-//     'Abr',
-//     'Mai',
-//     'Jun',
-//     'Jul',
-//     'Ago',
-//     'Set',
-//     'Out',
-//     'Nov',
-//     'Dez',
-//   ],
-//   dayNames: ['Domingo', 'Segunda', 'Terça', 'Quarta', 'Quinta', 'Sexta', 'Sábado'],
-//   dayNamesShort: ['Dom', 'Seg', 'Ter', 'Qua', 'Qui', 'Sex', 'Sáb'],
-//   today: 'Hoje',
-// };
-
-// LocaleConfig.defaultLocale = 'br';
-
-
-
-
-// function generateEmptyDates(startDate, endDate, agendamento) {
-//   const emptyDates = {};
-//   const currentDate = new Date(startDate);
-
-//   while (currentDate <= endDate) {
-//     const formattedDate = currentDate.toISOString().split('T')[0];
-
-//     // Adiciona apenas os dias com serviços agendados
-//     if (agendamento.some(item => item.attributes.data === formattedDate)) {
-//       emptyDates[formattedDate] = [];
-//     }
-
-//     currentDate.setDate(currentDate.getDate() + 1);
-//   }
-
-//   return emptyDates;
-// }
-
-
-
-
-// export default function TelaAgenda() {
-//   const [agendamento, setagendamento] = useState([]);
-//   const [atualizaLista, setAtualizaLista] = useState(true);
-//   const [modalVisible, setModalVisible] = useState(false);
-//   const [excluidoModalVisible, setExcluidoModalVisible] = useState(false);
-//   const [modalVisible2, setModalVisible2] = useState(false);
-//   const [excluidoModalVisible2, setExcluidoModalVisible2] = useState(false);
-//   const [items, setItems] = useState({});
-//   const [noAppointmentsMessage, setNoAppointmentsMessage] = useState(false); 
-
-//   const [selectedDate, setSelectedDate] = useState(new Date()); // Definir a data selecionada como o dia atual
-
-//   const navigation = useNavigation();
-
-//   useEffect(() => {
-//       axios
-//         .get('https://back-graciano--claragoncalves3.repl.co/api/agendamentos/?populate=*', configAxios)
-//         .then(function (response) {
-//           setagendamento(response.data.data);
-//           setAtualizaLista(false);
-//         })
-//         .catch(error => {
-//           console.log(error);
-//         });
-//     }, [atualizaLista]);
-
-
-//   useEffect(() => {
-//     const startDate = new Date(); // Defina a data de início, se desejar
-//     const endDate = new Date(); // Defina a data de término, se desejar
-//     endDate.setDate(endDate.getDate() + 365);
-//     const newItems = generateEmptyDates(startDate, endDate, agendamento);
-
-
-//     agendamento.forEach((item) => {
-//       const date = item.attributes.data; //  "data" = 'YYYY-MM-DD'
-//       if (!newItems[date]) {
-//         newItems[date] = [];
-//       }
-//       newItems[date].push({
-//         // name: `Cliente: ${item.attributes.cliente.data.attributes.nome}`,
-//         // hour: `Hora: ${item.attributes.hora}`
-//         name: `${item.attributes.cliente?.data?.attributes?.nome || 'Cliente Desconhecido'}`,
-//         data: `${item.attributes.data}`,
-//         hora: `${item.attributes.hora}`,
-//       });
-//     });
-
-//     setItems(newItems);
-//   }, [agendamento]); // analisar
-
-//   const toggleModal = () => {
-//     setModalVisible(!modalVisible);
-//   };
-
-//   const mostrarMensagemExcluido = () => {
-//     setExcluidoModalVisible(true);
-//     toggleModal();
-//   };
-
-//   const toggleModal2 = () => {
-//     setModalVisible2(!modalVisible2);
-//   };
-
-//   const mostrarMensagemExcluido2 = () => {
-//     setExcluidoModalVisible2(true);
-//     toggleModal2();
-//   };
-
-//   const loadItems = (day) => {
-//     setTimeout(() => {
-//       // Verificar se há itens para o dia
-//       if (!items[day.dateString] || items[day.dateString].length === 0) {
-//         setNoAppointmentsMessage(true);
-//       } else {
-//         setNoAppointmentsMessage(false);
-//       }
-//     }, 1);
-//   };
-  
-   
-
-//   const renderItem = (item) => {
-    
-//     return (
-//       <View>
-//         <TouchableOpacity style={{ marginRight: 10, marginTop: 20 }} activeOpacity={0.7} onPress={() => navigation.navigate("visualizar", { data: item, setAtualizaLista })}>
-//           <Card>
-//             <Card.Content>
-//               <View
-//                 style={{
-//                   flexDirection: 'row',
-//                   justifyContent: 'space-between',
-//                   alignItems: 'center',
-//                 }}>
-//                 <View>
-//                   <Text style={styles.textbotao3}>{item.name}</Text>
-//                   <Text style={styles.textbotao3}>{item.hour}</Text>
-//                 </View>
-//                 <TouchableOpacity activeOpacity={0.7} onPress={toggleModal}>
-//                   <Feather
-//                     name="trash-2"
-//                     color="#2D82B5"
-//                     size={22}
-//                     style={{ alignSelf: 'center' }}
-//                   />
-//                 </TouchableOpacity>
-//               </View>
-//             </Card.Content>
-//           </Card>
-//         </TouchableOpacity>
-//         <View style={[styles.button, styles.menu]}>
-//           <TouchableOpacity onPress={() => navigation.navigate('ClienteAdicionar')}>
-//             <Ionicons name="ios-person-add-outline" size={28} color="#015C92" />
-//           </TouchableOpacity>
-//         </View>
-//       </View>
-//     );
-//   };
-
-//   const calendarTheme = {
-//     calendarBackground: '#88CDF6', 
-//     dayTextColor: '#FFF',
-//     todayTextColor: '#FFF', 
-//     selectedDayBackgroundColor: '#015C92',
-//     selectedDayTextColor: '#88CDF6', 
-//     dotColor: '#015C92',
-//     textSectionTitleColor: "#FFF",
-//     monthTextColor: "#FFF",
-//     agendaKnobColor: '#015C92', // Cor do botão para carregar mais eventos
-//     selectedDay: {
-//       backgroundColor: '#88CDF6', // Cor de fundo dos dias com eventos
-//       borderRadius: 0,
-//     },
-//     'stylesheet.calendar.main': {
-//       emptyDayContainer: {
-//         backgroundColor: '#f0f0f0', // Cor de fundo para dias sem serviços
-//       },
-//     },
-//   };
-
-//   const renderEmptyItem = () => <View style={styles.emptyItem} />;
-
-//   return (
-//     <View style={{ flex: 1}}>
-//       <View style={styles.detalhe}>
-//         <Text style={styles.text1}>Agenda</Text>
-//       </View>
-//       <Agenda
-//         items={items}
-//         renderItem={renderItem}
-//         theme={calendarTheme}
-//         ListFooterComponent={renderEmptyItem}
-//         hideKnob={false}
-//         selected={selectedDate} 
-//         onDayPress={(day) => {
-//           setSelectedDate(new Date(day.dateString));
-//           loadItems(day);
-//         }}
-//       />
-      
-
-//       <View>
-//         <Modal
-//           animationType="slide"
-//           transparent={true}
-//           visible={modalVisible}
-//           onRequestClose={toggleModal}>
-//           <StatusBar backgroundColor="rgba(0, 0, 0, 0.5)" translucent={true} />
-//           <View style={styles.modalContainer}>
-//             <View style={styles.modalContent}>
-//               <Text style={styles.textbotao}>Deseja excluir esse serviço?</Text>
-//               <View style={styles.bots}>
-//                 <TouchableOpacity style={styles.bot} onPress={mostrarMensagemExcluido}>
-//                   <Text style={styles.textbotao2} >Sim</Text>
-//                 </TouchableOpacity>
-//                 <TouchableOpacity style={styles.bot2} onPress={toggleModal}>
-//                   <Text style={styles.textbotao} >Cancelar</Text>
-//                 </TouchableOpacity>
-//               </View>
-//             </View>
-//           </View>
-//         </Modal>
-//       </View>
-
-//       <Modal
-//         animationType="slide"
-//         transparent={true}
-//         visible={excluidoModalVisible}
-//         onRequestClose={() => setExcluidoModalVisible(false)}>
-//         <StatusBar backgroundColor="rgba(0, 0, 0, 0.5)" translucent={true} />
-//         <View style={styles.modalContainer}>
-//           <View style={styles.modalContent}>
-//             <Text style={styles.textbotao}>Serviço excluído com sucesso!</Text>
-//             <TouchableOpacity
-//               style={styles.bot3}
-//               onPress={() => setExcluidoModalVisible(false)}>
-//               <Text style={styles.textbotao2}>Fechar</Text>
-//             </TouchableOpacity>
-//           </View>
-//         </View>
-//       </Modal>
-//     </View>
-//   );
-// };
-
-// const styles = StyleSheet.create({
-//   textbotao: {
-//     fontSize: 14,
-//     color: 'white',
-//     fontFamily: 'Urbanist_900Black',
-//     textAlign: 'center',
-//   },
-  
-//   textbotao2: {
-//     fontSize: 14,
-//     color: '#379BD8',
-//     fontFamily: 'Urbanist_900Black',
-//     textAlign: 'center',
-//   },
-
-//   textbotao3: {
-//     fontSize: 14,
-//     color: '#015C92',
-//     fontFamily: 'Urbanist_900Black',
-//   },
-
-//   modalContainer: {
-//     flex: 1,
-//     justifyContent: 'center',
-//     alignItems: 'center',
-//     backgroundColor: 'rgba(0, 0, 0, 0.5)',
-//   },
-//   modalContent: {
-//     backgroundColor: '#379BD8',
-//     margin: 20,
-//     width: 280,
-//     height: 140,
-//     borderRadius: 20,
-//     padding: 35,
-//     elevation: 5,
-//   },
-//   bot:{
-//     width: 50,
-//     height: 30,
-//     backgroundColor: '#fff',
-//     borderRadius: 10,
-//     alignItems: 'center',
-//     justifyContent: 'center',
-//     alignSelf: 'center'
-//   },
-//   bot2:{
-//     width: 80,
-//     height: 30,
-//     borderWidth: 2,
-//     borderRadius: 10,
-//     borderColor: '#fff',
-//     alignItems: 'center',
-//     justifyContent: 'center'
-//   },
-//   bot3:{
-//     width: 80,
-//     height: 30,
-//     backgroundColor: '#fff',
-//     borderRadius: 10,
-//     alignItems: 'center',
-//     justifyContent: 'center',
-//     alignSelf: 'center',
-//     marginTop: 20
-//   },
-//   bots:{
-//     flexDirection: 'row',
-//     justifyContent: 'space-between',
-//     marginHorizontal: 20,
-//     marginTop: 20
-//   },
-//   detalhe: {
-//     backgroundColor: '#88CDF6',
-//     posistion: 'absolute',
-//     paddingLeft: 20,
-//     paddingBottom: 10,
-//     paddingTop: 10,
-//     paddingRight: 20,
-//   },
-//   text1: {
-//     fontSize: 30,
-//     fontFamily: 'Urbanist_900Black',
-//     color: '#015C92',
-//     marginTop: 20,
-//   },
-//   emptyItem: {
-//     height: 2000, 
-//   },
-//   button: {
-//     right: 20,
-//     bottom: 80,
-//     zIndex: 1,
-//     position: 'absolute',
-//     width: 60,
-//     height: 60,
-//     borderRadius: 60 / 2,
-//     justifyContent: 'center',
-//     alignItems: 'center',
-//     shadowColor: '#88CDF6',
-//     shadowRadius: 10,
-//     shadowOpacity: 0.3,
-//     shadowOffset: {
-//       height: 10,
-//     },
-//   },
-//   menu: {
-//     backgroundColor: '#88CDF6',
-//   },
-// });
