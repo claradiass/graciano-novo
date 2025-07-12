64f2b4b701898e57b1ffb1b96c70d5e9db7bc273
dinizhugo <hugodiniztech01@gmail.com>
2025-07-11 21:50:35 -0300

    feat: Criando a constante de lista opçoes de aparelhos

--- util/constantes.js
+++ util/constantes.js
@@ -12,3 +12,11 @@ export const baseUrlClientesPaginado = (page, pageSize) => {
     return baseUrl + 'graciano-clientes/' + `?pagination[start]=${(page-1)*pageSize}&pagination[limit]=${pageSize}`
 };
 export const baseUrlAgendamentos = baseUrl + 'graciano-agendamentos/';
+
+export const listaOpcoes = [
+  {label: 'Selecionar', value: ''},
+  {label: 'Ar-condicionado', value: 'Ar-condicionado'},
+  {label: 'Geladeira', value: 'Geladeira'},
+  {label: 'Freezer', value: 'Freezer'},
+  {label: 'outros', value: 'outros'},
+];
